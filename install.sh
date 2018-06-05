f083830e3e59fbfffb00f595ec6f48bfd65a88d0
Magic-Star <39926562+Magic-Star@users.noreply.github.com>
2018-06-05 16:56:19 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-read -p "Do you want me to Bot? (Y/n): " q
+read -p "آیا برای نصب ربات مجیک استار اطمینان دارید؟ (Y/n): " q
 
 if [ "$q" == "y" ] || [ "$q" == "Y" ] || [ "$q" == "" ]; then
-    echo -e "\e[1;36mInstalling\e[0m"
+    echo -e "\e[1;36mدرحال نصب پیکیج ها...\e[0m"
     #sudo apt install python-setuptools
     #sudo apt install python-pip
     #sudo apt install python-redis
     #sudo pip install pyTelegramBotAPI
 
-    echo -e "\e[1;36mUpdating pyTelegram\e[0m"
+    echo -e "\e[1;36mدرحال نصب پیکیج ها ...\e[0m"
     #sudo pip install pyTelegramBotAPI --upgrade
     #sudo apt update
  
-    echo -e "\e[1;36mInstalling python 2.7 and Modules\e[0m"
+    echo -e "\e[1;36mدرحال نصب پایتون 2.7 و ماژول ها\e[0m"
     #sudo apt install python2.7
     #sudo pip install pytelegrambotapi py==1.4.29 pytest==2.7.2 requests==2.7.0 six==1.9.0 wheel==0.24.0
-  
-
-    
-    echo -e "\e[1;36mInstalling\e[0m"
     chmod +x launch.sh
-    echo -e "\e[1;32mInstalling successfully installed! Change values in config file and run ./launch.sh\e[0m"
+    echo -e "\e[1;32mنصب با موفقیت انجام شد ! هم اکنون میتوانید مجیک استار را اجرا کنید.\e[0m"
     echo " "
 fi
